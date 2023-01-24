b2bcfefc5681c47d3075828e54336dc673f44ac1
pedro tesch <pedrotesch.pt@gmail.com>
2023-01-23 21:00:14 -0300

    its working :)))

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # todoList
 A To-Do list to improve my knowledge about HTML CSS and JS!!!
-The website is <a href="https://fastidious-salamander-6c5d4b.netlify.app">here</a>
+You can access the website clicking right <a href="https://fastidious-salamander-6c5d4b.netlify.app">here</a>
--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,38 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 let oldInputValue;
 
+let items = JSON.parse(localStorage.getItem("array")) || []
+
 //Funções
-const saveTodo = text =>{
+const saveTodo = (text, isDone) =>{
+    
     const todo = document.createElement("div");
     todo.classList.add("todo");
-
+    
     const todoTitle = document.createElement("h3");
     todoTitle.innerText = text;
     todo.appendChild(todoTitle);
-
+    
     const doneBtn = document.createElement("button")
     doneBtn.classList.add("finish-todo")
     doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
     todo.appendChild(doneBtn)
-
+    
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-todo");
     editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
     todo.appendChild(editBtn);
-
+    
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     todo.appendChild(deleteBtn);
-
+    
     todoList.appendChild(todo);
+    
+    if(isDone == "true")
+        todo.classList.toggle("done");
+
 
     todoInput.value = "";
     todoInput.focus();
@@ -47,64 +54,90 @@ const toggleForms = () => {
 
 const updateTodo = text => {
     const todos = document.querySelectorAll(".todo");
-
+    
     todos.forEach(todo => {
         let todoTitle = todo.querySelector("h3")
-
+        localStorage.setItem(text, "false");
+        localStorage.removeItem(oldInputValue);
         if(todoTitle.innerText === oldInputValue){
             todoTitle.innerText = text;
         }
-    })
+    });
+}
+
+const getInfos = () => {
+    let storage = {...localStorage}
+    console.log(storage);
+    storage = Object.keys(storage).sort().reduce((r, k) => (r[k] = storage[k], r), {});
+    console.log(storage);
+    todoList.innerHTML = "";
+    for(const prop in storage)
+        saveTodo(prop, storage[prop]);
+    
 }
 
 //Eventos
 todoForm.addEventListener("submit", e =>{
     e.preventDefault();
 
-    const inputValue = todoInput.value 
-
+    const inputValue = todoInput.value
+    
     if(inputValue){
-        saveTodo(inputValue);
+        items.push(inputValue)
+        localStorage.setItem(inputValue, false);
     }
+    getInfos();
 });
 
 document.addEventListener("click", e =>{
     const targetEl = e.target;
     const parentEl = targetEl.closest("div");
     let todoTitle;
-
+    
     if(parentEl && parentEl.querySelector("h3"))
-        todoTitle = parentEl.querySelector("h3").innerText;
-
+    todoTitle = parentEl.querySelector("h3").innerText;
+    
     if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done");
-    }
 
+        if(parentEl.classList.contains("done"))
+            localStorage.setItem(todoTitle, "true");
+        else
+            localStorage.setItem(todoTitle, "false");
+    }
+    
     if(targetEl.classList.contains("remove-todo")){
         parentEl.remove();
+        localStorage.removeItem(todoTitle);
     }
-
+    
     if(targetEl.classList.contains("edit-todo")){
         toggleForms();
         editInput.value = todoTitle;
         oldInputValue = todoTitle;
     }
+
 });
 
 cancelEditBtn.addEventListener("click", e => {
     e.preventDefault();
-
+    
     toggleForms();
 });
 
 editForm.addEventListener("submit", e => {
     e.preventDefault();
-
+    
     const editInputValue = editInput.value
-
+    
     if(editInputValue){
         updateTodo(editInputValue);
     }
-
+    
     toggleForms();
 });
+
+
+window.onload = () => {
+    getInfos();
+}
